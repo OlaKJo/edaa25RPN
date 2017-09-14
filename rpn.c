59cfdd90a1894abcc0e66de4739153a884a95ee1
Ola Johansson <ola.oj.johansson@gmail.com>
2017-09-14 11:59:39 +0200

    small updated to rpn.c

--- rpn.c
+++ rpn.c
@@ -19,7 +19,7 @@ void Stack_Init(Stack *S)
 int Stack_Top(Stack *S)
 {
 	if (S->size == 0) {
-		fprintf(stderr, "Error: stack empty\n");
+		//fprintf(stderr, "Error: stack empty\n");
 		return -1;
 	}
 
@@ -30,14 +30,14 @@ void Stack_Push(Stack *S, int d)
 {
 	if (S->size < STACK_MAX)
 		S->data[S->size++] = d;
-	else
-		fprintf(stderr, "Error: stack full\n");
+	else;
+		//fprintf(stderr, "Error: stack full\n");
 }
 
 void Stack_Pop(Stack *S)
 {
-	if (S->size == 0)
-		fprintf(stderr, "Error: stack empty\n");
+	if (S->size == 0);
+		//fprintf(stderr, "Error: stack empty\n");
 	else
 		S->size--;
 }
@@ -46,6 +46,7 @@ void Stack_Pop(Stack *S)
 int main(void)
 {
 	int a;
+	int line = 1;
 	int accum_num = 0;
 	struct Stack* stack = malloc(sizeof(struct Stack));
 	int numerator, denominator, term1, term2 = 0;
@@ -60,51 +61,54 @@ int main(void)
 			//printf("the number %d was pushed to stack\n", (accum_num));
 			Stack_Push(stack, accum_num);
 			accum_num = 0;
+		} else {
+
+			switch (a)
+				{
+					case '+':
+						//printf("+\n");
+						term1 = Stack_Top(stack);
+						Stack_Pop(stack);
+						term2 = Stack_Top(stack);
+						Stack_Pop(stack);
+
+						Stack_Push(stack, term1 + term2);
+						break;
+
+					case '-':
+						//printf("-\n");
+						term1 = Stack_Top(stack);
+						Stack_Pop(stack);
+						term2 = Stack_Top(stack);
+						Stack_Pop(stack);
+
+						Stack_Push(stack, term2 - term1);
+						break;
+
+					case '*':
+						term1 = Stack_Top(stack);
+						Stack_Pop(stack);
+						term2 = Stack_Top(stack);
+						Stack_Pop(stack);
+						Stack_Push(stack, term1 * term2);
+						//printf("*\n");
+						break;
+					case '/':
+						denominator = Stack_Top(stack);
+						Stack_Pop(stack);
+						numerator = Stack_Top(stack);
+						Stack_Pop(stack);
+						Stack_Push(stack, numerator / denominator);
+						//printf("/\n");
+						break;
+					case '\n':
+						printf("Line %d: %d\n", line++, Stack_Top(stack));
+						Stack_Pop(stack);
+						break;
+					default:
+						printf("Line %d: error at %c", line++, a);
+				}
+			//printf("you pressed: %d\n", isdigit(a));
 		}
-
-		switch (a)
-			{
-				case '+':
-					//printf("+\n");
-					term1 = Stack_Top(stack);
-					Stack_Pop(stack);
-					term2 = Stack_Top(stack);
-					Stack_Pop(stack);
-
-					Stack_Push(stack, term1 + term2);
-					break;
-
-				case '-':
-					//printf("-\n");
-					term1 = Stack_Top(stack);
-					Stack_Pop(stack);
-					term2 = Stack_Top(stack);
-					Stack_Pop(stack);
-
-					Stack_Push(stack, term2 - term1);
-					break;
-
-				case '*':
-					term1 = Stack_Top(stack);
-					Stack_Pop(stack);
-					term2 = Stack_Top(stack);
-					Stack_Pop(stack);
-					Stack_Push(stack, term1 * term2);
-					//printf("*\n");
-					break;
-				case '/':
-					denominator = Stack_Top(stack);
-					Stack_Pop(stack);
-					numerator = Stack_Top(stack);
-					Stack_Pop(stack);
-					Stack_Push(stack, numerator / denominator);
-					//printf("/\n");
-					break;
-				case '\n':
-					printf("result is: %d\n", Stack_Top(stack));
-					Stack_Pop(stack);
-					break;
-			}
-		//printf("you pressed: %d\n", isdigit(a));
 	}
 }
