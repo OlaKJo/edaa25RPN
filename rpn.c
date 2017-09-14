5bdc37adae8f5b34a411d3cce5ca8f767b5e1dc6
Ola Johansson <ola.oj.johansson@gmail.com>
2017-09-14 10:53:54 +0200

    edit rpn to read characters with getchar and only recognize valid chars - placeholder prints, no arithmetic

--- rpn.c
+++ rpn.c
@@ -1,6 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 #define N		(10)
+
 
 int main(void)
 {
-	return 0;
+	int a;
+	int accum_num;
+	int stack_counter = 0;
+
+
+	while(1) {
+		a = getchar();
+		while (isdigit(a) != 0) {
+			accum_num = accum_num * 10 + (a-'0');
+			a = getchar();
+		}
+		if (accum_num != 0) {
+			printf("the number %d was pushed to stack\n", (accum_num));
+			accum_num = 0;
+		}
+
+		switch (a)
+			{
+				case '+':
+					printf("+\n");
+					break;
+				case '-':
+					printf("-\n");
+					break;
+				case '*':
+					printf("*\n");
+					break;
+				case '/':
+					printf("/\n");
+					break;
+				case '\n':
+					printf("calculating\n");
+					break;
+			}
+		//printf("you pressed: %d\n", isdigit(a));
+	}
 }
