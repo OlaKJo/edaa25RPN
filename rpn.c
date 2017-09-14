8ba16ebc7778ed152a555b6dad12ae56364811f3
Ola Johansson <ola.oj.johansson@gmail.com>
2017-09-14 11:17:13 +0200

    added a stack struct and methods

--- rpn.c
+++ rpn.c
@@ -1,15 +1,54 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+
 #define N		(10)
+#define STACK_MAX 100
+
+struct Stack {
+	int		data[STACK_MAX];
+	int		size;
+};
+typedef struct Stack Stack;
+
+void Stack_Init(Stack *S)
+{
+	S->size = 0;
+}
+
+int Stack_Top(Stack *S)
+{
+	if (S->size == 0) {
+		fprintf(stderr, "Error: stack empty\n");
+		return -1;
+	}
+
+	return S->data[S->size-1];
+}
+
+void Stack_Push(Stack *S, int d)
+{
+	if (S->size < STACK_MAX)
+		S->data[S->size++] = d;
+	else
+		fprintf(stderr, "Error: stack full\n");
+}
+
+void Stack_Pop(Stack *S)
+{
+	if (S->size == 0)
+		fprintf(stderr, "Error: stack empty\n");
+	else
+		S->size--;
+}
 
 
 int main(void)
 {
 	int a;
-	int accum_num;
+	int accum_num = 0;
 	int stack_counter = 0;
-
+	struct Stack* stack = malloc(sizeof(struct Stack));
 
 	while(1) {
 		a = getchar();
